2cf1201a49f90c8be864507d92aa4e4898d1f150
chenhao-stick-to <3301273828@qq.com>
2024-01-08 23:45:05 +0800

    linux内核设计与实现完成

--- leetcode/array/27.移除元素.cpp
+++ leetcode/array/27.移除元素.cpp
@@ -9,8 +9,8 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int size = 0;
-        int ksize=nums.size();
-        for(int i = 0;i < ksize;++i){
+        int ksize = nums.size();
+        for (int i = 0;i < ksize;++i) {
             if(val == nums[i]){
                 nums.erase(nums.begin()+i);
                 continue;
