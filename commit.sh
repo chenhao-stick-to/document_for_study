acdd1de28445cae80fe83a9e0f33351ec4ad8f9d
chenhao-stick-to <3301272828@qq.com>
2024-01-08 17:51:27 +0800

    linux内核设计与实现完成

--- commit.sh
+++ commit.sh
@@ -1,3 +1,3 @@
 git add . 
-git commit -m "linux内核"
+git commit -m "linux内核设计与实现完成"
 git push origin main
---
+++ 其他/python环境.md
@@ -0,0 +1,2 @@
+# anocond环境
+conda create 无法创建环境时，一般都是condarc出了问题。可以删除channels下的镜像源路径，修改以下；添加代理等。
