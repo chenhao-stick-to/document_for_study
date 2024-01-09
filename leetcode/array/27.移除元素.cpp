0746627bfeb6cd6e1200a9b3130cd10dd0bc74e6
chenhao-stick-to <3301272828@qq.com>
2024-01-09 18:02:36 +0800

    linux内核设计与实现完成

--- leetcode/array/27.移除元素.cpp
+++ leetcode/array/27.移除元素.cpp
@@ -9,15 +9,33 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int size = 0;
-        int ksize = nums.size();
-        for (int i = 0;i < ksize;++i) {
-            if(val == nums[i]){
-                nums.erase(nums.begin()+i);
-                continue;
+        int ksize = nums.size()-1;
+        // for (int i = 0;i < ksize;++i) {
+        //     if(val == nums[i]){
+        //         nums.erase(nums.begin()+i);
+        //         continue;
+        //     }
+        //     size++;
+        // }
+        while (size <= ksize) {
+            if (val != nums[size] && nums[ksize] == val) {
+                size++;
+                ksize--;
+            } else if(val == nums[size] && nums[ksize] == val) {
+                ksize--;
+            } else if (val !=nums[size] && nums[kszie] != val) {
+                size++;
+            } else {
+                int temp = nums[size];
+                nums[size] = nums[kszie];
+                nums[ksize] = temp;
+                kszie--;
+                size++;
             }
-            size++;
         }
-        return size;
+        if (nums[size] != val)
+            size -= 1;
+        return size+1;
     }
 };
 // @lc code=end
--- 其他/python环境.md
+++ 其他/python环境.md
@@ -1,2 +1,7 @@
 # anocond环境
 conda create 无法创建环境时，一般都是condarc出了问题。可以删除channels下的镜像源路径，修改以下；添加代理等。
+# pip 
+pip install -r requirements.txt  --proxy=http://10.252.1.248:8889
+pip install出现了
+WARNING: Retrying (Retry(total=2, connect=None, read=None, redirect=None, status=None)) after connection broken by 'NewConnectionError('<pip._vendor.urllib3.connection.HTTPConnection object at 0x7feb6b9d49d0>: Failed to establish a new connection: [Errno 101] Network is unreachable')': /pypi/simple/protobuf/错误。
+更换镜像源无法解决，需要配置代理，如上解决。
