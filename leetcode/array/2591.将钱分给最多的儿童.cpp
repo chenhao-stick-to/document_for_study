139b17fea6c6b8858593248c48fd8a157a700940
chenhao-stick-to <3301273828@qq.com>
2024-01-15 20:21:12 +0800

    linux内核设计与实现完成

---
+++ leetcode/array/2591.将钱分给最多的儿童.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=2591 lang=cpp
+ *
+ * [2591] 将钱分给最多的儿童
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int distMoney(int money, int children) {
+        
+    }
+};
+// @lc code=end
+
