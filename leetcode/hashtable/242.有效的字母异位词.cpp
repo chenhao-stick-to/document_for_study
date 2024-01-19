1c5e6259344a0b09f496ae9f8c6518f4396752fe
chenhao-stick-to <3301273828@qq.com>
2024-01-19 18:11:20 +0800

    leetcode

--- leetcode/hashtable/242.有效的字母异位词.cpp
+++ leetcode/hashtable/242.有效的字母异位词.cpp
@@ -8,7 +8,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
+        std::map<char,int> maps;//unicode可采用宽字符和字符串
+        std::map<char,int> mapt;//注意map是key有序，可顺序比较。
+        for (int i = 0; i < s.size(); i++) {
+            maps[s[i]]++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            mapt[t[i]]++;
+        }
+        auto iters = maps.begin();
+        auto itert = mapt.begin();
+        while (iters != maps.end() && itert != mapt.end()) {
+            if (iters->first == itert->first && iters->second == itert->second) {
+                iters++;
+                itert++;
+                continue;
+            }
+            return false;
+        }
+        if (iters == maps.end() && itert == mapt.end()) {
+            return true;
+        }
+        return false;
     }
 };
 // @lc code=end
---
+++ leetcode/hashtable/349.两个数组的交集.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=cpp
+ *
+ * [349] 两个数组的交集
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> hash_vector(1000,0);
+
+    }
+};
+// @lc code=end
+
