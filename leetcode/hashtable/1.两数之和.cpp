9c652a0cbd66eef6955811ced0248053480a0c9f
chenhao-stick-to <3301273828@qq.com>
2024-01-22 20:29:18 +0800

    leetcode

--- leetcode/hashtable/1.两数之和.cpp
+++ leetcode/hashtable/1.两数之和.cpp
@@ -7,26 +7,21 @@
 // @lc code=start
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {//使用map。
+    vector<int> twoSum(vector<int>& nums, int target) {//使用map。不适用map复杂度是n方，使用则是nlogn.
         multimap <int,int> hash_map;//key是nums的值，value是对应的索引。
         vector <int> result;
         for (int i = 0; i < nums.size(); i++) {
-            hash_map[nums[i]] = i;
+            // hash_map[nums[i]] = i;
+            hash_map.insert({nums[i],i});
         }
-        for (auto it = hash_map.begin(); it !=hash_map.end(); ++it) {
-            if (it->first > target) {
-                continue;
+        for (auto it = hash_map.begin(); it !=hash_map.end(); ++it) {//n的复杂度
+            if (hash_map.find(target-it->first) != hash_map.end() && (hash_map.find(target-it->first))->second != it->second) {//logn的复杂度
+                result.push_back(it->second);
+                result.push_back((hash_map.find(target-it->first))->second);
+                return result;
             }
-            int index = target;
-            while (index > 0) {
-                if (hash_map.find(index) != hash_map.end()) {
-                    result.push_back(hash_map[index]);
-                    break;
-                }
-                index -= 
-        
             }
-        
+            return result;
         }
 };
 // @lc code=end
